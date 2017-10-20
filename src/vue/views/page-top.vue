b9bb3269eb6bbd853f95a55bbe4822d0f5faf18e
y-eioki <sena.rn68@gmail.com>
2017-10-20 20:28:59 +0900

    hotfix

--- src/vue/views/page-top.vue
+++ src/vue/views/page-top.vue
@@ -115,7 +115,7 @@
         continuityFlg: false,
         searchCount: 0,
         reSearchLimitTime: 10000,
-        reSearchLimitCount: 5,
+        reSearchLimitCount: 10,
         stateMessage: {
           0: '30文字以上の検索はできません',
           1: '短時間に連続しての検索はできません',
